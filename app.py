b74222cadf80cae8c36f8a2131b26e61c0dc26ef
Deyson Urrego <deysonurrego159@gmail.com>
2025-07-08 12:30:43 -0500

    Metodo para verificar la conexion

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+from data.conexion import Conexion
+
+# Crear una instancia de la conexión
+conexion = Conexion(db_name='viviendas_s_a', collection_name='vivienda')
+
+# Verificar si la conexión fue exitosa
+if conexion.conexion_exitosa:
+    print("Conexión a MongoDB verificada correctamente.")
+else:
+    print("La conexión a MongoDB falló.")
 
 
 
--- data/conexion.py
+++ data/conexion.py
@@ -1,13 +1,30 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 
 class Conexion:
     def __init__(self, db_name, collection_name):
-        self.cliente = MongoClient('mongodb://localhost:27017/')
-        self.db = self.cliente[db_name] 
-        self.collection = self.db[collection_name] 
+        try:
+            self.cliente = MongoClient('mongodb://localhost:27017/')  
+            self.db = self.cliente[db_name]
+            self.collection = self.db[collection_name]
+            self.conexion_exitosa = True
+            print(" Conexión exitosa a MongoDB.")
+        
+        except errors.ServerSelectionTimeoutError as e:
+            self.conexion_exitosa = False
+            print("No se pudo conectar a MongoDB. Verifica que el servidor esté en ejecución.")
+            print(f"Error: {e}")
 
-    def insertar(self, document):
-        self.collection.insert_one(document)
+    def insertar(self, documento):
+        if self.conexion_exitosa:
+            self.collection.insert_one(documento)
+        else:
+            print("No se puede insertar: conexión no establecida.")
 
     def obtener_viviendas(self):
-        return list(self.collection.find())
+        if self.conexion_exitosa:
+            return list(self.collection.find())
+        else:
+            print("No se pueden obtener datos: conexión no establecida.")
+            return []
+
+
